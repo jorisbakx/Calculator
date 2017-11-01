62d1ee63d2812e4994a695d64369f264e394a885
Joris Bakx <jjbakx@zeelandnet.nl>
2017-11-01 14:23:38 +0100

    must and should haves aangemaakt

---
+++ Calculator/Calculator2/Musthaves.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+	class Musthaves: CalculatorMusthaves
+	{
+		public int Diff(int getal1, int getal2)
+		{
+			int getal3 = getal1 - getal2;
+			return getal3;
+		}
+
+		public int Product(int getal1, int getal2)
+		{
+			int getal3 = getal1 * getal2;
+			return getal3;
+		}
+
+		public int Quotient(int getal1, int getal2)
+		{
+			int getal3 = getal1 / getal2;
+			return getal3;
+		}
+
+		public int Sum(int getal1, int getal2)
+		{
+			int getal3 = getal1 + getal2;
+			return getal3;
+		}
+	}
+}
---
+++ Calculator/Calculator2/Shouldhaves.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+	class Shouldhaves : CalculatorShouldhaves;
+	{
+		public int Max(int getal1, int getal2)
+		{
+			if ( getal1 > getal2)
+				return getal1;
+			else return getal2;
+			
+		}
+
+		public int Min(int getal1, int getal2)
+		{
+			if (getal1 < getal2)
+				return getal1;
+			else return getal2;
+		}
+
+		public int Rest(int getal1, int getal2)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
